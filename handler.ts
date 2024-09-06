d1780cda25be4cddfe8ba0606c0c28ea5cb16b9d
Gabriel Bruno <64500546+gabrielbcsilva@users.noreply.github.com>
2024-09-06 14:56:49 -0300

    refactor: Adding new functions

--- handler.ts
+++ handler.ts
@@ -1,8 +1,33 @@
-export const hello = async (event) => {
+import { APIGatewayEvent } from "aws-lambda";
+
+export const hello = async (event: APIGatewayEvent) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "Go Serverless v4! Your function executed successfully!",
+      queryString: event.queryStringParameters
     }),
   };
 };
+
+export const user = async (event: APIGatewayEvent) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      name: "Gabriel",
+      location:"Brasil",
+      age:18,
+      queryString: event.queryStringParameters
+    }),
+  };
+};
+
+export const post = async (event: APIGatewayEvent) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      body: event.body
+    }),
+  };
+};
+
